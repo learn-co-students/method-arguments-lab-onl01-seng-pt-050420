fc5af19d73c9166ef4cb2ff9f1c06cc611cc3c73
Deborah Santillan <s.debb@yahoo.com>
2020-05-16 16:51:22 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,16 +1,39 @@
-```ruby
-# lib/introduction.rb
-
-def introduction(name)
+def introduction(name) 
   puts "Hi, my name is #{name}."
-end
-```
-
-```ruby
-# lib/introduction.rb
+end 
 
 def introduction_with_language(name, language)
-  puts `"Hi, my name is #{name}" and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}." 
 end
-``` 
+
+# <<<<<<< HEAD
+# ```ruby
+# # lib/introduction.rb
+# =======
+# def introduction_with_language
+# end 
+# >>>>>>> 25bcca9d6f45b9392f9ab3d1bc33247536572b54
+
+# def introduction(name)
+#   puts "Hi, my name is #{name}."
+# end
+# <<<<<<< HEAD
+# ```
+
+# ```ruby
+# # lib/introduction.rb
+# =======
+
+# def  introduction_with_language(name, language)
+# end
+# >>>>>>> 25bcca9d6f45b9392f9ab3d1bc33247536572b54
+
+# def introduction_with_language(name, language)
+#   puts `"Hi, my name is #{name}" and I am learning to program in #{language}."
+# end
+# <<<<<<< HEAD
+# ``` 
+# =======
+
+# >>>>>>> 25bcca9d6f45b9392f9ab3d1bc33247536572b54
   
